eacb5e014443b7cda70ed24c9f792a1dfbe54ee7
Pawel <pawel.losek@outlook.com>
2021-06-22 23:14:50 +0200

    frontend dockerfile

--- react/frontend/src/components/table/AppTableWrapper.tsx
+++ react/frontend/src/components/table/AppTableWrapper.tsx
@@ -5,12 +5,7 @@ import TableCell from "@material-ui/core/TableCell";
 import TableBody from "@material-ui/core/TableBody";
 import TableContainer from "@material-ui/core/TableContainer";
 import {makeStyles} from "@material-ui/core/styles";
-import {GenericColumn} from "../util/GenericColumn";
-import Table from '@material-ui/core/Table';
 
-type TableProps<T = any> = PropsWithChildren<{
-    columns: GenericColumn<T>[]
-}>
 
 const useStyles = makeStyles({
     root: {
@@ -27,32 +22,13 @@ const useStyles = makeStyles({
     }
 });
 
-const Table: FC<TableProps> = (props) => {
+const Table: FC<{}> = (props) => {
 
     const classes = useStyles();
 
     return (
         <div className={classes.root}>
-            <TableContainer className={classes.container}>
-                <Table>
-                    <TableHead>
-                        <TableRow>
-                            {props.columns.map((column) => (
-                                <TableCell
-                                    key={column.id}
-                                    align={column.align}
-                                    style={{minWidth: column.minWidth}}
-                                >
-                                    {column.label}
-                                </TableCell>
-                            ))}
-                        </TableRow>
-                    </TableHead>
-                    <TableBody>
-                        {props.children}
-                    </TableBody>
-                </Table>
-            </TableContainer>
+
         </div>
     )
 }
