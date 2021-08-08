da78da0a9e9b9a0471123508e98746ef324df8fa
Pawel <pawel.losek@outlook.com>
2021-08-08 20:44:39 +0200

    Simple login flow

--- react/frontend/src/App.tsx
+++ react/frontend/src/App.tsx
@@ -1,44 +1,18 @@
 import './App.css';
-import { BrowserRouter, Route, Redirect, Switch} from 'react-router-dom';
-import Drawer from "./components/Drawer";
-import Header from "./components/Header";
-import { AuthProvider, useAuthState } from './context/auth/context';
-import { PublicRoutes, SecuredRoutes } from './config/routes';
-import React, { Component } from 'react';
-import Login from './components/login/Login';
+import { BrowserRouter } from 'react-router-dom';
+import { AuthProvider } from './context/auth/context';
+import Routing from './components/routing/Routing';
 
 function App() {
-
-    const { loggedIn } = useAuthState();
-
     return (
-    <div className="App">
-        <AuthProvider>
-            <BrowserRouter>
-            { loggedIn && <Drawer/> && <Header></Header> }
-
-            { loggedIn ? 
-            (<Switch>
-                { 
-                SecuredRoutes.map(route => (
-                    <Route exact={route.exact} path={route.path} component={route.component} ></Route>
-                ))
-                }
-                 <Redirect to='/'/>
-            </Switch>) :
-            (<Switch>
-                {
-                    PublicRoutes.map(route => (
-                        <Route exact={route.exact} path={route.path} component={route.component} ></Route>
-                    ))
-                }
-                <Redirect exact to='/login' />
-            </Switch>) }
-
-            </BrowserRouter>
-        </AuthProvider>
-    </div>
-  );
+        <div className="App">
+            <AuthProvider>
+                <BrowserRouter>
+                    <Routing></Routing>
+                </BrowserRouter>
+            </AuthProvider>
+        </div>
+    );
 }
 
 export default App;
---
+++ react/frontend/src/components/routing/Routing.tsx
@@ -0,0 +1,43 @@
+import React, { FC } from "react";
+import { Redirect, Route, Switch } from "react-router-dom";
+import { PublicRoutes, SecuredRoutes } from "../../config/routes";
+import { useAuthState } from "../../context/auth/context";
+import Drawer from "../Drawer";
+import Header from "../Header";
+
+type RoutingProps = {}
+
+const Routing: FC<RoutingProps> = () => {
+
+    const { loggedIn } = useAuthState();
+
+    return (
+        <div>
+            {loggedIn && <Drawer /> && <Header></Header>}
+            <Switch>
+                {loggedIn ?
+                    (
+
+                        SecuredRoutes.map(route => (
+                            <Route exact={route.exact} path={route.path} component={route.component} ></Route>
+                        )).concat(
+                            <Redirect to='/' />
+                        )
+
+                    ) :
+                    (
+
+                        PublicRoutes.map(route => (
+                            <Route exact={route.exact} path={route.path} component={route.component} ></Route>
+                        ))
+                            .concat(
+                                <Redirect exact to='/login' />
+                            )
+
+                    )}
+            </Switch>
+        </div>
+    )
+}
+
+export default Routing;
