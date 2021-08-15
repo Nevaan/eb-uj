4a093ce8bdf87ae8eb2a9ca3667e24bed2e6bfc3
Pawel <pawel.losek@outlook.com>
2021-08-15 11:25:25 +0200

    Request logging

---
+++ scala/backend/app/filter/LoggingFilter.scala
@@ -0,0 +1,34 @@
+package filter
+
+import javax.inject.Inject
+import akka.util.ByteString
+import play.api.Logging
+import play.api.libs.streams.Accumulator
+import play.api.mvc._
+import scala.concurrent.ExecutionContext
+
+class LoggingFilter @Inject() (implicit ec: ExecutionContext) extends EssentialFilter with Logging {
+  def apply(nextFilter: EssentialAction) = new EssentialAction {
+    def apply(requestHeader: RequestHeader) = {
+      val startTime = System.currentTimeMillis
+
+      val accumulator: Accumulator[ByteString, Result] = nextFilter(requestHeader)
+
+      accumulator.map { result =>
+        val endTime     = System.currentTimeMillis
+        val requestTime = endTime - startTime
+
+        logger.info(
+          s"${requestHeader.method} ${requestHeader.uri} took ${requestTime}ms and returned ${result.header.status}"
+        )
+
+        logger.info("Request headers: ")
+        requestHeader.headers.headers.map {
+          x => logger.info(s"\t${x}")
+        }
+
+        result
+      }
+    }
+  }
+}
